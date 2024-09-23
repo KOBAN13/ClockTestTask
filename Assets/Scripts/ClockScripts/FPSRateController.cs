3fe354225e3d9f02f914c263a19583a9fa0b4633
KOBAN13 <layk.danil@mail.ru>
2024-09-23 18:14:44 +0400

    добавил ограничение фпс для часов

---
+++ Assets/Scripts/ClockScripts/FPSRateController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Client
+{
+    public class FPSRateController : MonoBehaviour
+    {
+        private const int FPS_RATE = 60;
+
+        private void Awake()
+        {
+            Application.targetFrameRate = FPS_RATE;
+        }
+    }
+}
